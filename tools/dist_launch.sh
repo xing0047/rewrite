fe5301d76ef35c9158d232c1bd03cf53235753a1
Xing Yun (邢云) <90847067+xing0047@users.noreply.github.com>
2023-10-21 21:32:20 +0800

    Create dist_launch.sh

---
+++ tools/dist_launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# -------------------------------------------------------------------------
+# Copyright (c) 2021-2022, NVIDIA Corporation & Affiliates. All rights reserved.
+#
+# This work is made available under the Nvidia Source Code License.
+# To view a copy of this license, visit
+# https://github.com/NVlabs/GroupViT/blob/main/LICENSE
+#
+# Written by Jiarui Xu
+# -------------------------------------------------------------------------
+
+
+SCRIPT=$1
+CONFIG=$2
+GPUS=$3
+PORT=${PORT:-29500}
+
+PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $SCRIPT --cfg $CONFIG ${@:4}
